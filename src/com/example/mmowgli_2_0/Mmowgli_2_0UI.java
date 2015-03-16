9681d46f22872c8239e6918e60adc5523be33d13
EricHenry <correia.eh@gmail.com>
2015-03-16 15:54:05 -0400

    Implemented first version of side bar, trending tags, and popular tags of main page

--- src/com/example/mmowgli_2_0/Mmowgli_2_0UI.java
+++ src/com/example/mmowgli_2_0/Mmowgli_2_0UI.java
@@ -4,8 +4,6 @@ import java.io.File;
 
 import javax.servlet.annotation.WebServlet;
 
-import com.example.mmowgli_2_0.DocEditor;
-import com.example.mmowgli_2_0.Mmowgli_2_0UI;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.data.Property.ValueChangeListener;
@@ -13,78 +11,52 @@ import com.vaadin.data.util.FilesystemContainer;
 import com.vaadin.data.util.TextFileProperty;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.HorizontalSplitPanel;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 @Theme("mmowgli_2_0")
 public class Mmowgli_2_0UI extends UI {
-
-//	@WebServlet(value = "/*", asyncSupported = true)
-//	@VaadinServletConfiguration(productionMode = false, ui = Mmowgli_2_0UI.class)
-//	public static class Servlet extends VaadinServlet {
-//	}
-//
-//	@Override
-//	protected void init(VaadinRequest request) {
-//		final VerticalLayout layout = new VerticalLayout();
-//		layout.setMargin(true);
-//		setContent(layout);
-//
-//		Button button = new Button("Click Me");
-//		button.addClickListener(new Button.ClickListener() {
-//			public void buttonClick(ClickEvent event) {
-//				layout.addComponent(new Label("Thank you for clicking"));
-//			}
-//		});
-//		layout.addComponent(button);
-//	}
+	
+	private FilesystemContainer doc = new FilesystemContainer(new File("C:\\Users\\Kilsuf"));
+//	ComboBox doclist = new ComboBox("Documents", doc);
+	//create a table instead of combobox
+	private Table doclist = new Table("Documents", doc);
+	//Label docView = new Label("", ContentMode.HTML); //replaced with Doceditor
+	private DocEditor docView = new DocEditor();
+	private SideBar staticbar = new SideBar();
+	private HomePageUpdates homePage = new HomePageUpdates();
 	
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = Mmowgli_2_0UI.class)
 	public static class Servlet extends VaadinServlet {
 	}
 	
-	FilesystemContainer doc = new FilesystemContainer(new File("C:\\Users\\Kilsuf"));
-//	ComboBox doclist = new ComboBox("Documents", doc);
-	//create a table instead of combobox
-	Table doclist = new Table("Documents", doc);
-	//Label docView = new Label("", ContentMode.HTML); //replaced with Doceditor
-	DocEditor docView = new DocEditor();
-	
 	
 	@Override
 	protected void init(VaadinRequest request) {
-//		final VerticalLayout layout = new VerticalLayout();
-//		layout.setMargin(true);
-//		setContent(layout);
-//
-//		Button button = new Button("Click Me");
-//		button.addClickListener(new Button.ClickListener() {
-//			public void buttonClick(ClickEvent event) {
-//				layout.addComponent(new Label("Thank you for clicking"));
-//			}
-//		});
-//		layout.addComponent(button);
 		
-		//setContent(doclist); //original way to add doclist to the screen
+		HorizontalLayout split = new HorizontalLayout();
+		//split.setWidth("70%");
 		
-		//Create a Panal that holds components to display on the screen
-		HorizontalSplitPanel split = new HorizontalSplitPanel();
+		split.setSizeFull();
 		
 		//Push split onto the screen
 		setContent(split);
 		
 		//add the doclist and docView as components
-		split.addComponent(doclist);
-		split.addComponent(docView);
+		split.addComponent(staticbar);
+		split.addComponent(homePage);
+		//split.addComponent(doclist);
+		//split.addComponent(docView);
 		//tell the doclist to use all the space that the split panel allows for
 		doclist.setSizeFull();
+		
+		//set the aspect ratio
+		split.setExpandRatio(staticbar, 0.1f);
+		//split.setExpandRatio(doclist, 1.0f);
+		split.setExpandRatio(homePage, 1.0f);
 		
 		//Event Handler
 		doclist.addValueChangeListener(new ValueChangeListener() {
